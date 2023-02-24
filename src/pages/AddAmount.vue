748c556b2f69360bb665999df5aa939e5948b0fc
Abdul Waheed <meharwaheed4313@gmail.com>
2023-02-24 16:01:18 +0500

    fixing

--- src/pages/AddAmount.vue
+++ src/pages/AddAmount.vue
@@ -4,10 +4,10 @@
       Add Amount
     </div>
     <div class="row q-pa-sm justify-center">
-      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-8">
+      <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
         <q-select filled v-model="model" :options="options" label="Select User" />
       </div>
-      <div class="col-xs-12 col-sm-12 col-md-4 col-lg-8 q-mt-md">
+      <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 q-mt-md">
         <q-input filled v-model="text" label="Amount" />
       </div>
       <div class="col-sm-12 col-xs-12 col-md-8 col-lg-8 q-mt-md">
